6a159ddc9080c6cd3df4cd0298a1bea227fa7cc3
André Kumagae <andre.kumagae@hotmail.com>
2021-04-02 22:29:06 -0300

    EXERCÍCIO 4-8
    Escreva um programa que calcule o somatório dos números inteiros entre os limites inferior e superior definidos pelo usuário

---
+++ livro python/cap 4/ex-04-08.py
@@ -0,0 +1,8 @@
+# s = int(input('Entre com o limite superior: '))
+# inf = int(input('Entre com o limite inferior: '))
+inf = -70
+s = -10
+somatorio = 0
+for i in range(inf, s+1):
+    somatorio += i
+print(somatorio)
