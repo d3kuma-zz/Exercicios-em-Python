7bf2e0668cbffc824c2505aa3b229ae5fe463486
André Kumagae <andre.kumagae@hotmail.com>
2021-04-05 17:54:30 -0300

    EXERCÍCIO 5 - Seleção

---
+++ livro python/cap5/ex-01-1.py
@@ -0,0 +1,2 @@
+for c in range(0, 256, 1):
+    print(f'{c} = {chr(c)}')
---
+++ livro python/cap5/ex-01-2.py
@@ -0,0 +1,5 @@
+x = int(input('Primeiro número: '))
+y = int(input('Segundo número: '))
+
+if x < y: print(f'{y} é o maior número.')
+else:  print(f'{x} é o maior número.')
